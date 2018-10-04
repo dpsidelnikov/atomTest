6d9b09e534bf666851dbacfd13f7cb400657a99c
dpsidelnikov <dpsidelnikov@gmail.com>
2018-10-04 22:50:35 +0200

    Some test that I've done!

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var number = 18;
+
+console.log(number);
+
+if (number > 12) {
+  console.log('Es mayor que 12')
+} else {
+  console.log('Es menor que 12');
+}
---
+++ pde/pde.pde
@@ -0,0 +1,8 @@
+void setup(){
+        size(640, 360);
+        pixelDensity(2);
+};
+
+void draw(){
+        ellipse(320,180,100,100);
+};
